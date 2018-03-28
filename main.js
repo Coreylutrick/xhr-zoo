cae78dd4993eee030f0bd4eb4da91fab4b5bf630
Corey Lutrick <coreylutrick@gmail.com>
2018-03-27 19:38:30 -0700

    added color change for type of animal

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const PrintToDom = (domString, divId) =>
 };
 
 // BUilds the string that is printed to the dom
-
 const buildDomString = (animalArray) => 
 {
   let domString = ""
@@ -20,31 +19,69 @@ const buildDomString = (animalArray) =>
     domString +=  `<h3>${animal.number}</h3>`;
     domString +=  `<img class="animalImg" src="${animal.imageUrl}" alt="Image of animal"><br>`;
     domString +=  `<div class="button-container">`;
-    domString +=    `<button class="card-button">Escaped</button>`;
+    domString +=    `<button class="escaped">Escaped</button>`;
     domString +=  `</div>`
     domString += `</div>`;
   });
   PrintToDom(domString, "animal-holder")
 };
 
-//grabs the data and makes the array that im pulling from
+const animalsEscaped = () => 
+{
+  console.log(showCarnivores);
+  showCarnivores();
+  showVegetables()
+};
 
+//Event Listener
+const addEscapedEventListener = () => 
+{
+  const escapedButtons = document.getElementsByClassName("escaped");
+  for(let i = 0; i<escapedButtons.length; i++)
+  {
+    escapedButtons[i].addEventListener("click", animalsEscaped)
+  }
+};
+
+//Escaped Animals function
+
+
+//Carnivore and vegetable functions
+const showCarnivores = () => 
+{
+  const carnivores = document.getElementsByClassName("carnivore");
+  for (let j = 0; j<carnivores.length; j++)
+  {
+    carnivores[j].children[4].innerHTML = "";
+    carnivores[j].classList.add("red");
+  }
+};
+
+const showVegetables = () => 
+{
+  const vegetables = document.getElementsByClassName("vegetable");
+  for (let j = 0; j<vegetables.length; j++)
+  {
+    vegetables[j].children[4].innerHTML = "";
+    vegetables[j].classList.add("green");
+  }
+};
+
+//grabs the data and makes the array that im pulling from
 function executeThisCodeAfterFileLoaded () 
 {
   const data = JSON.parse(this.responseText);
   buildDomString(data.animals);
-
+  addEscapedEventListener();
 }
 
 // only runs if there is an error
-
 function executeThisCodeIfXHRFails () 
 {
   console.log("something went wrong");
 }
 
 //function that runs the application to make it work
-
 const startApp = () => 
 {
   let myRequest = new XMLHttpRequest();
